622d68f374db8a452aa4a5b099b86e70028b51aa
RicheyT-PFW <Richtj03@pfw.edu>
2026-03-11 14:40:22 +0000

    Finishing BinaryTree study session using Animal.java

--- edu/pfw/richtj03/study/ch9/Animal.java
+++ edu/pfw/richtj03/study/ch9/Animal.java
@@ -3,7 +3,7 @@ package edu.pfw.richtj03.study.ch9;
 import java.util.Scanner;
 
 public class Animal {
-	private static Scanner stdin = new Scanner(System.in);
+	private static final Scanner stdin = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		BTNode<String> root;
@@ -17,70 +17,78 @@ public class Animal {
 		System.out.println("Thanks for teaching me a thing or two.");	
 	}
 
-	//This method prints instructions explaning the game
+//This method prints instructions explaning the game
 	public static void instruct(){
 		System.out.println
-			("An animal guessing game that gets smarter and smarter!");	  
+			("An animal guessing game that gets smarter and smarter!\n-------------------------------------------------------");
+		System.out.println("");	  
 	}
-/**
- * learn (for the Animal-Guessing Program)
-	public static void learn(BTNode<String> current)
-	Elicit information from the user to improve the binary taxonomy tree.
-	Parameter:
-	  current – a reference to a leaf node of a binary taxonomy tree
-	Precondition:
-	  current is a reference to a leaf in a binary taxonomy tree.
-	Postcondition:
-	  Information has been elicited from the user, and the tree has been
-	  improved.
-	Throws: OutOfMemoryError
-	  Indicates that there is insufficient memory to add information to the tree
- **/
-	public static void learn(BTNode current) {
+
+
+// The learn Method. This method is activated when the game reaches a leaf and makes a wrong guess. 
+// The method takes several steps to improve the taxonomy tree. The method’s argument is a reference 
+// to the node that contains the incorrect guess
+
+/***  public static void learn(BTNode<String> current)
+ *	Elicit information from the user to improve the binary taxonomy tree.
+ *	Parameter:
+ *	  current – a reference to a leaf node of a binary taxonomy tree
+ *	Precondition:
+ *	  current is a reference to a leaf in a binary taxonomy tree.
+ *	Postcondition:
+ *	  Information has been elicited from the user, and the tree has been
+ *	  improved.
+ *	Throws: OutOfMemoryError
+ *	  Indicates that there is insufficient memory to add information to the tree
+ */ public static void learn(BTNode<String> current) {
 				
 	}
 
-/** The play Method. The play method has one parameter, which initially is a
-reference to the root of the binary taxonomy tree, as shown in this specification:
- * play (for the Animal-Guessing Program)
-	public static void play(BTNode<String> current)
-	Play one round of the animal-guessing game.
+//The play Method. The play method has one parameter, which initially is a
+//reference to the root of the binary taxonomy tree
 
-	Parameter:
-	   current – a reference to the root node of a binary taxonomy tree that
-	   will be used to play the game
-
-	Postcondition:
-	   The method has played one round of the game and possibly added new
-	   information about a new animal.
-	Throws: OutOfMemoryError
-	   Indicates that there is insufficient memory to add information to the tree.
- **/
-	public static void play(BTNode current) {
-		while (!current.isLeaf( )){
-			if (query(current.getData( )))
-				current = current.getLeft( );
+/*** public static void play(BTNode<String> current)
+*	Play one round of the animal-guessing game.
+*
+*	Parameter:
+*	   current – a reference to the root node of a binary taxonomy tree that
+*	   will be used to play the game
+*
+*	Postcondition:
+*	   The method has played one round of the game and possibly added new
+*	   information about a new animal.
+*	Throws: OutOfMemoryError
+*	   Indicates that there is insufficient memory to add information to the tree.
+*/ 	public static void play(BTNode<String> current) {
+		while (!current.isLeaf()){
+			if (query(current.getData()))
+				current = current.getLeft();
 			else
-				current = current.getRight( );
+				current = current.getRight();
 		}
-		System.out.print("My guess is " + current.getData( ) + ". ");
+		System.out.print("My guess is " + current.getData() + ". ");
 		if (!query("Am I right?"))
 			learn(current);
 		else
 			System.out.println("I knew it all along!");
 	}
 
-	public static boolean query(String str) {
+// The query Method. This method prints a prompt (using System.out.print)
+// and reads the user’s yes or no answer (using stdin.nextLine). If the user
+// responds yes, then the method returns true; otherwise, the method returns false.
+	public static boolean query(String prompt) {
 		String answer;
 		System.out.print(prompt + " [Y or N]: ");
-		answer = stdin.nextLine( ).toUpperCase( );
+		answer = stdin.nextLine().toUpperCase();
 		while (!answer.startsWith("Y") && !answer.startsWith("N")){
 			System.out.print("Invalid response. Please type Y or N: ");
-			answer = stdin.nextLine( ).toUpperCase( );
+			answer = stdin.nextLine().toUpperCase();
 		}
 		return answer.startsWith("Y");	
 	}
 
+// The beginningTree Method. This method creates the initial binary taxonomy tree 
+// and returns a reference to the root of this tree
 	public static BTNode<String> beginningTree() {
 		BTNode<String> root;
 		BTNode<String> child;
@@ -89,7 +97,7 @@ reference to the root of the binary taxonomy tree, as shown in this specificatio
 		// 1. Make root refer to a new node with the data "Array you a mammal?".
 		// Both children initially null.
 		final String ROOT_QUESTION = "Are you a mammal";
-		root = new BTNode<String>(null, null, ROOT_QUESTION);
+		root = new BTNode<>(null, null, ROOT_QUESTION);
 		
 		//TODO
 		// 2. Make child refer to a new node with the data "Are you bigger than a cat?".
@@ -98,9 +106,9 @@ reference to the root of the binary taxonomy tree, as shown in this specificatio
 		final String LEFT_QUESTION = "Are you bigger than a cat?";
 		final String ANIMAL1 = "Kangaroo";
 		final String ANIMAL2 = "Mouse";
-		child = new BTNode<String>(null, null, LEFT_QUESTION);
-		child.setLeft(new BTNode<String>(null, null, ANIMAL1));
-		child.setRight(new BTNode<String>(null, null, ANIMAL2));
+		child = new BTNode<>(null, null, LEFT_QUESTION);
+		child.setLeft(new BTNode<>(null, null, ANIMAL1));
+		child.setRight(new BTNode<>(null, null, ANIMAL2));
 		root.setLeft(child);
 
 		//TODO
@@ -110,12 +118,11 @@ reference to the root of the binary taxonomy tree, as shown in this specificatio
 		final String RIGHT_QUESTION = "Do you live underwater?";
 	        final String ANIMAL3 = "Trout";
 		final String ANIMAL4 = "Robin";
-		child = new BTNode<String>(null, null, RIGHT_QUESTION);
-		child.setLeft(new BTNode<String>(null, null, ANIMAL3));
-		child.setRight(new BTNode<String>(null, null, ANIMAL4));
+		child = new BTNode<>(null, null, RIGHT_QUESTION);
+		child.setLeft(new BTNode<>(null, null, ANIMAL3));
+		child.setRight(new BTNode<>(null, null, ANIMAL4));
 		root.setRight(child);	
 		
 		return root;
-
 	}
 }
