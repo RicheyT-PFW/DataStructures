0437c9995965ebd99365a96685fbad61b63bb5be
RicheyT-PFW <Richtj03@pfw.edu>
2026-03-16 21:27:13 +0000

    Update BinaryTree.java

--- edu/pfw/richtj03/study/ch9/BinaryTree.java
+++ edu/pfw/richtj03/study/ch9/BinaryTree.java
@@ -2,7 +2,7 @@ package edu.pfw.richtj03.study.ch9;
 
 public class BinaryTree<E> {
 
-	private class  Node<E> {
+	private class Node<E> {
 	   E data;
 	   Node<E> left;
 	   Node<E> right;
@@ -12,6 +12,31 @@ public class BinaryTree<E> {
 		this.right = right;
 		this.data = data;
 	   }
+
+	   private E getData() {
+			return this.data; 
+	   }
 	}
+
+	public BinaryTree(E data) {
+		root = new Node<>(null, null, data);
+		cursor = root;
+    }
+
+
+	
+	private Node<E> cursor;
+	private Node<E> root;
+	private int nodeCount;
+	private int childCount;
+	private int leafCount;
+	private int depth;
+
+	public static void main(String[] args) {
+		BinaryTree<Integer> tree = new BinaryTree<>(3);
+		System.out.println(tree.root.getData());
+
+	}
+
 }
 
